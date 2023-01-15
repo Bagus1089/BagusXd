56f4451d249d1b9e4dbc4098af0a190639fc044b
Bagus1089 <alwaysad1.11.2@gmail.com>
2023-01-15 15:35:49 +0700

    Update info-totalfitur.js

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -3,13 +3,13 @@ let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
- await conn.sendButton(m.chat, `Total Fitur Bot Saat ini: ${totalf}\n`,wm + '\n\n' + botdate, giflogo, [['MENU','.menu']], m, {
+ await conn.sendButton(m.chat, `Total Fitur Saat ini: ${totalf}\n`,wm + '\n\n' + botdate, giflogo, [['MENU','.menu']], m, {
 contextInfo: { externalAdReply :{
                         mediaUrl: '',
                         mediaType: 2,
                         description: 'anu',
                         title: bottime,
-                        body: 'Total Cintaku Padamu',          previewType: 0,
+                        body: 'Total Fitur',          previewType: 0,
                         thumbnail: fs.readFileSync("./thumbnail.jpg"),
                         sourceUrl: sig
                       }}
